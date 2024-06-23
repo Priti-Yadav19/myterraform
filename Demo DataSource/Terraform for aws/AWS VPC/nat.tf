4e3e4ad0877f5fd0500fd8e44915f80be592841a
Priti Yadav <pritiyadav1909@gmail.com>
2024-06-23 22:40:03 +0530

    error rectified

--- Demo DataSource/Terraform for aws/AWS VPC/nat.tf
+++ Demo DataSource/Terraform for aws/AWS VPC/nat.tf
@@ -1,10 +1,11 @@
-#define external ip
+# Define external IP
 resource "aws_eip" "my_nat_eip" {
-  domain   = "vpc"
+  domain = "vpc"
 }
 
+# Define NAT Gateway
 resource "aws_nat_gateway" "mynatgw" {
-  allocation_id = aws_eip.my_nat_eip
+  allocation_id = aws_eip.my_nat_eip.id
   subnet_id     = aws_subnet.public_subnet1.id
 
   tags = {
@@ -12,29 +13,32 @@ resource "aws_nat_gateway" "mynatgw" {
   }
 }
 
+# Define Route Table
 resource "aws_route_table" "my_rt" {
   vpc_id = aws_vpc.myvpc.id
 
   route {
-    cidr_block = "10.0.0.0/0"
-    gateway_id = aws_internet_gateway.mynatgw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.mynatgw.id
   }
- tags = {
+
+  tags = {
     Name = "my_rt"
   }
 }
 
-resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.private_subnet1.id
-  route_table_id = aws_route_table.my_rt
-}
-
-resource "aws_route_table_association" "rta3" {
-  subnet_id      = aws_subnet.private_subnet2.id
-  route_table_id = aws_route_table.my_rt
-}
-
+# Route Table Associations
 resource "aws_route_table_association" "rta4" {
+  subnet_id      = aws_subnet.private_subnet1.id
+  route_table_id = aws_route_table.my_rt.id
+}
+
+resource "aws_route_table_association" "rta5" {
+  subnet_id      = aws_subnet.private_subnet2.id
+  route_table_id = aws_route_table.my_rt.id
+}
+
+resource "aws_route_table_association" "rta6" {
   subnet_id      = aws_subnet.private_subnet3.id
-  route_table_id = aws_route_table.my_rt
+  route_table_id = aws_route_table.my_rt.id
 }
